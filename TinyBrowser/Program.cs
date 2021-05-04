ee3b8c062a7d9d7e0b581d3e96022c4bdd447d5b
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-05-04 13:37:46 +0200

    Basic program that makes a request, the request right now is a bad request

---
+++ TinyBrowser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TinyBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "acme.com";
+            var uri = "/";
+            Int32 port = 80;
+            var tcpClient = new TcpClient(host, port);
+            var stream = tcpClient.GetStream();
+            var request = Encoding.ASCII.GetBytes("GET {uri} HTTP/1.1\r\nHost: {host}\r\n\r\n");
+            stream.Write(request, 0, request.Length);
+            var reader = new StreamReader(stream);
+            var readToEnd = reader.ReadToEnd();
+            Console.WriteLine(readToEnd);
+            tcpClient.Close();
+            stream.Close();
+        }
+    }
+}
