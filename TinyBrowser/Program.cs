a7f9b1f74b009b45ffea73f9365da8ccfaa51519
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-05-04 18:50:47 +0200

    bad request now fixed

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -15,7 +15,7 @@ namespace TinyBrowser
             Int32 port = 80;
             var tcpClient = new TcpClient(host, port);
             var stream = tcpClient.GetStream();
-            var request = Encoding.ASCII.GetBytes("GET {uri} HTTP/1.1\r\nHost: {host}\r\n\r\n");
+            var request = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\nHost: acme.com\r\n\r\n");
             stream.Write(request, 0, request.Length);
             var reader = new StreamReader(stream);
             var readToEnd = reader.ReadToEnd();
